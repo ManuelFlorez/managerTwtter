2a6f85baf3f0911e63c9d984d5ed7eeae08565b5
Manuel Florez <manuelflorezw@gmail.com>
2021-07-11 09:54:08 -0500

    base twitter

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,10 @@
-import { Consultar as Facebook } from './services/facebook/Consultar'
-import { Consultar as Twitch } from './services/twitch/Consultar'
-import { Consultar as Twitter } from './services/twitter/Consultar'
-import { Consultar as Youtube } from './services/youtube/Consultar'
+import { Consultar as Twitter } from './services/Consultar'
 import { config } from 'dotenv'
 
 config();
 
 const twitter = new Twitter().twitter()
-const twitch = new Twitch().twitch()
-const facebook = new Facebook().facebook()
-const youtube = new Youtube().youtube()
 
 twitter.subscribe(subscribe => {
-  console.log(subscribe.urlPerfil);
-});
-
-twitch.subscribe(subscribe => {
-  console.log(subscribe.urlPerfil);
-});
-
-facebook.subscribe(subscribe => {
-  console.log(subscribe.urlPerfil);
-});
-
-youtube.subscribe(subscribe => {
   console.log(subscribe.urlPerfil);
 });
---
+++ src/services/Consultar.ts
@@ -0,0 +1,19 @@
+import { SocialNetwork } from "../SocialNetworks"
+import { generate } from 'shortid'
+import { Observable } from 'rxjs'
+
+export class Consultar {
+  socialNetwork: SocialNetwork;
+
+  constructor() {
+    this.socialNetwork = {
+      id: generate(),
+      name: 'Twitter',
+      urlPerfil: ''
+    }
+  }
+
+  public twitter() :Observable<SocialNetwork> {
+    
+  }
+}
